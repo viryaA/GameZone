96c330d20cc26509b3d90d00fd4da00da0256b39
affanhafidz <crashshot15@gmail.com>
2025-07-10 08:45:16 +0700

    m

--- Navigation/UserTabs.js
+++ Navigation/UserTabs.js
@@ -3,7 +3,7 @@ import RentalHome from '../screens/Pelanggan/Rental/RentalHome';
 import "../global.css"
 import MasterlHeader from "../Header/MasterlHeader";
 import DetailRentalHome from "../screens/Transaction/DetailRentail/DetailRentalHome";
-import ProfileScreen from "../screens/Pelanggan/ProfileScreenPelanggan";
+import ProfileScreen from "../screens/Admin/ProfileScreenAdmin";
 
 const Stack = createNativeStackNavigator()
 
